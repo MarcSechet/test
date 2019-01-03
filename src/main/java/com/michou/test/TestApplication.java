39ae93bb79ac8a2f8ff9dcfd2984792888d95b79
Marc Sechet <marc.sechet@capgemini.com>
2019-01-03 15:25:19 +0100

    Initial commit

---
+++ src/main/java/com/michou/test/TestApplication.java
@@ -0,0 +1,18 @@
+package com.michou.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@SpringBootApplication
+public class TestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestApplication.class, args);
+	}
+
+}
+
